82292928d486582ae7e0be8f37e983e61d6ffe50
eyeless <maxim.chevychelov@gmail.com>
2022-11-06 16:06:04 +0500

    listen и disconnect

--- ChatServer/ChatServer.cs
+++ ChatServer/ChatServer.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace ChatServer
 {
@@ -7,11 +10,12 @@ namespace ChatServer
     {
         static TcpListener tcpListener;
         List<ClientObject> clients = new List<ClientObject>();
- 
+
         protected internal void AddConnection(ClientObject clientObject)
         {
             clients.Add(clientObject);
         }
+
         protected internal void RemoveConnection(string id)
         {
             // получаем по id закрытое подключение
@@ -20,4 +24,41 @@ namespace ChatServer
             if (client != null)
                 clients.Remove(client);
         }
+
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //отключение клиента
+            }
+
+            Environment.Exit(0); //завершение процесса
+        }
+    }
 }
