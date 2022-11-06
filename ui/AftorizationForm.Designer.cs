e762f4aaa95d2da4e06f638dfada3427392ae6e0
akulaok <polovina62003@gmail.com>
2022-11-06 18:43:18 +0500

    готовая форма

---
+++ ui/AftorizationForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace chat
+{
+    partial class AftorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(140)))), ((int)(((byte)(158)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(242, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(119, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(432, 46);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.name.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(140)))), ((int)(((byte)(158)))));
+            this.name.Location = new System.Drawing.Point(39, 68);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(74, 29);
+            this.name.TabIndex = 2;
+            this.name.Text = "Name";
+            // 
+            // AftorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(643, 269);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "AftorizationForm";
+            this.Text = "Name";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label name;
+    }
+}
---
+++ ui/ChatForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace chat
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatForm));
+            this.SendBotton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.String = new System.Windows.Forms.RichTextBox();
+            this.massages = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SendBotton
+            // 
+            this.SendBotton.BackColor = System.Drawing.Color.SteelBlue;
+            this.SendBotton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SendBotton.BackgroundImage")));
+            this.SendBotton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SendBotton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.SendBotton.FlatAppearance.BorderSize = 0;
+            this.SendBotton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SendBotton.Location = new System.Drawing.Point(742, 412);
+            this.SendBotton.Margin = new System.Windows.Forms.Padding(0);
+            this.SendBotton.Name = "SendBotton";
+            this.SendBotton.Size = new System.Drawing.Size(100, 60);
+            this.SendBotton.TabIndex = 0;
+            this.SendBotton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.SendBotton.UseVisualStyleBackColor = false;
+            this.SendBotton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // String
+            // 
+            this.String.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.String.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.String.Location = new System.Drawing.Point(2, 412);
+            this.String.Name = "String";
+            this.String.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.String.Size = new System.Drawing.Size(742, 60);
+            this.String.TabIndex = 1;
+            this.String.Text = "";
+            // 
+            // massages
+            // 
+            this.massages.AutoSize = true;
+            this.massages.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.massages.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.massages.Location = new System.Drawing.Point(2, 3);
+            this.massages.Name = "massages";
+            this.massages.Size = new System.Drawing.Size(0, 29);
+            this.massages.TabIndex = 2;
+            this.massages.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(140)))), ((int)(((byte)(158)))));
+            this.ClientSize = new System.Drawing.Size(840, 472);
+            this.Controls.Add(this.massages);
+            this.Controls.Add(this.String);
+            this.Controls.Add(this.SendBotton);
+            this.Name = "ChatForm";
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendBotton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox String;
+        private System.Windows.Forms.Label massages;
+    }
+}
+
